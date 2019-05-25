e047429a15dfc2ccab23611e9459e222887af335
Carlos Delgado <cardel@debian>
2019-05-24 22:53:31 -0500

    first

---
+++ nqueens.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*- 
+import numpy as np
+
+
+
+
+#La solución es un arreglo y la función de fitnees es que tanto se cruzan las reinas.
+
+def fitness(poblacion):
+	
+	return fitnees
+	
+def algoritmo(n)
+	mejor_individuo = 0
+	
+	
+	return mejor_individuo
+
+
+n = 8
+solucion = algoritmo(n)
